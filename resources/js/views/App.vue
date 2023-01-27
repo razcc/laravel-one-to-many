3c55929ca76b497bc9b517ab93bf8095ae7aa8d8
Rafael Leonardi <rafael.leonardi98@gmail.com>
2023-01-27 12:25:45 +0100

    Divisione Laravel-Vue

---
+++ resources/js/views/App.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1>Work in progress</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name:"App",
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ resources/views/guest/home.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Guest Home</title>
+</head>
+
+<body>
+    <!-- inseriamo un unico div #root a cui si aggancerà vue -->
+    <div id="root"> </div>
+
+    <!-- colleghiamo il file js -->
+    <script src="{{ asset('js/app.js') }}" charset="utf-8"></script>
+</body>
+
+</html>
